4e7c24a886118ef55f1d3bf53ea54b76aa092a95
Jacek Suty <jsuty@softblue.pl>
2021-03-06 17:15:15 +0100

    Dodano klasę DEV2

---
+++ ConsoleApp1/DEV2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class DEV2
+    {
+        public string Autor { get; } = "Jacek Suty/Mikołaj Dalecki";
+    }
+}
