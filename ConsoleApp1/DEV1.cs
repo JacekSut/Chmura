e08536e175c1a57e73901b21c341511153de3a0f
Przemek Nadobny <prznad005@utp.edu.pl>
2021-03-06 17:16:49 +0100

    dodanie klasy DEV1

---
+++ ConsoleApp1/DEV1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class DEV1
+    {
+        public string Autor { get; } = "Przemysław Nadobny";
+
+    }
+}
